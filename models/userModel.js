654685b5e3d772610f240dd0c2a93e321436ec68
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2022-12-30 20:27:31 +0545

    reset token method added

--- models/userModel.js
+++ models/userModel.js
@@ -94,3 +94,19 @@ userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
     // False means NOT changed
     return false;
   };
+
+  userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+  
+    this.passwordResetToken = crypto
+      .createHash('sha256')
+      .update(resetToken)
+      .digest('hex');
+  
+    console.log({ resetToken }, this.passwordResetToken);
+  
+    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+  
+    return resetToken;
+  };
+  

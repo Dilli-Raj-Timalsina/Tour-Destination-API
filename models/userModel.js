c2ec29240d630e00380dfed290810ae84786f4a2
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2022-12-30 20:26:46 +0545

    chagepadsswordafter method added

--- models/userModel.js
+++ models/userModel.js
@@ -80,3 +80,17 @@ userSchema.methods.correctPassword = async function(
   ) {
     return await bcrypt.compare(candidatePassword, userPassword);
   };
+
+userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
+    if (this.passwordChangedAt) {
+      const changedTimestamp = parseInt(
+        this.passwordChangedAt.getTime() / 1000,
+        10
+      );
+  
+      return JWTTimestamp < changedTimestamp;
+    }
+  
+    // False means NOT changed
+    return false;
+  };

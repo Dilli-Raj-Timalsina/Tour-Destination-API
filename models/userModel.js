53232a68823bd929709e8ac1f849811d6dd7e0da
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-01-11 17:56:27 +0545

    correctpassword middleware added

--- models/userModel.js
+++ models/userModel.js
@@ -60,18 +60,6 @@ userSchema.pre('save', async function(next) {
     next();
   });
   
-userSchema.pre('save', function(next) {
-    if (!this.isModified('password') || this.isNew) return next();
-  
-    this.passwordChangedAt = Date.now() - 1000;
-    next();
-  });
-  
-  userSchema.pre(/^find/, function(next) {
-    // this points to the current query
-    this.find({ active: { $ne: false } });
-    next();
-  });
   
   
 userSchema.methods.correctPassword = async function(
@@ -81,34 +69,6 @@ userSchema.methods.correctPassword = async function(
     return await bcrypt.compare(candidatePassword, userPassword);
   };
 
-userSchema.methods.changedPasswordAfter = function(JWTTimestamp) {
-    if (this.passwordChangedAt) {
-      const changedTimestamp = parseInt(
-        this.passwordChangedAt.getTime() / 1000,
-        10
-      );
-  
-      return JWTTimestamp < changedTimestamp;
-    }
-  
-    // False means NOT changed
-    return false;
-  };
-
-  userSchema.methods.createPasswordResetToken = function() {
-    const resetToken = crypto.randomBytes(32).toString('hex');
-  
-    this.passwordResetToken = crypto
-      .createHash('sha256')
-      .update(resetToken)
-      .digest('hex');
-  
-    console.log({ resetToken }, this.passwordResetToken);
-  
-    this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
-  
-    return resetToken;
-  };
 
   const User = mongoose.model('User', userSchema);
 

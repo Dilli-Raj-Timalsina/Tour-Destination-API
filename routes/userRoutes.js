6d5ab10a5aa0f4af1f79f506a37b7ac578e0e93d
Dilli Raj Timalsina <dillirajtimalsina354@gmail.com>
2023-01-11 17:52:21 +0545

    signup and login route implemented

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,17 +1,10 @@
 const express = require('express');
-const userController = require('./../controllers/userController');
+const authController = require('./../controllers/authController.js');
 
 const router = express.Router();
 
-router
-  .route('/')
-  .get(userController.getAllUsers)
-  .post(userController.createUser);
+router.post('/signup',authController.signup);
+router.post('/login',authController.login);
 
-router
-  .route('/:id')
-  .get(userController.getUser)
-  .patch(userController.updateUser)
-  .delete(userController.deleteUser);
 
 module.exports = router;
